b3dbb46b514b7ced5e50e60235c6119dcc4f7c1e
Vadim Gamzaev <Vadosbox@gmail.com>
2022-09-24 19:59:23 +0300

    Initial Commit

---
+++ PhoneBookSwiftUI/PhoneBookSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneBookSwiftUIApp.swift
+//  PhoneBookSwiftUI
+//
+//  Created by Вадим Гамзаев on 24.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct PhoneBookSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
